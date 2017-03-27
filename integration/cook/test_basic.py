9526ee2643b0cd63832266fc57a5e8824b861666
Paul Schorfheide <paul.schorfheide@twosigma.com>
2017-03-27 12:30:10 -0500

    Add session

--- integration/cook/test_basic.py
+++ integration/cook/test_basic.py
@@ -9,13 +9,14 @@ class CookTest(unittest.TestCase):
 
     @retry(stop_max_delay=120000, wait_fixed=5000)
     def wait_for_job(self, job_id, status):
-        job = requests.get('%s/rawscheduler?job=%s' % (self.cook_url, job_id))
+        job = self.session.get('%s/rawscheduler?job=%s' % (self.cook_url, job_id))
         self.assertEqual(200, job.status_code)
         job = job.json()
         self.assertEqual(status, job[0]['status'])
 
     def setUp(self):
         self.cook_url = 'http://localhost:12321'
+        self.session = requests.Session()
 
 
     def test_basic_submit(self):
@@ -29,6 +30,6 @@ class CookTest(unittest.TestCase):
             'name': 'echo',
             'priority': 1
         }]}
-        resp = requests.post('%s/rawscheduler' % self.cook_url, json=request_body)
+        resp = self.session.post('%s/rawscheduler' % self.cook_url, json=request_body)
         self.assertEqual(resp.status_code, 201)
         self.wait_for_job(job_uuid, 'completed')
