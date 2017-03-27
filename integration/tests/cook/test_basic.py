9860341211a277e96cce67ca0e0b738e302c14c9
Steven Radack <sradack@gmail.com>
2017-03-27 16:03:41 -0500

    checkpoint

--- integration/tests/cook/test_basic.py
+++ integration/tests/cook/test_basic.py
@@ -73,11 +73,17 @@ class CookTest(unittest.TestCase):
         self.assertEqual(1, len(job['instances']))
         self.assertEqual('failed', job['instances'][0]['status'])
 
-    def test_query_job_correct_keys_in_response(self):
+    def test_get_job(self):
+        # schedule a job
         job_spec = self.minimal_job() 
         request_body = {'jobs': [job_spec]}
-        resp = self.session.post('%s/rawscheduler' % self.cook_url, json=request_body)
-        self.assertEqual(resp.status_code, 201)
+        resp = self.session.post('%s/rawscheduler' % self.cook_url,
+                                 json={'jobs': [job_spec]})
+        self.assertEqual(201, resp.status_code)
+
+        # query for the same job & ensure the response has what it's supposed to have
+        resp = self.session.get('%s/rawscheduler?job=%s' % (self.cook_url, job_spec['uuid']))
+        self.assertEqual(200, resp.status_code)
         job = self.wait_for_job(job_spec['uuid'], 'completed')
         self.assertEquals(job_spec['mem'], job['mem'])
         self.assertEquals(job_spec['max_retries'], job['max_retries'])
@@ -108,6 +114,87 @@ class CookTest(unittest.TestCase):
         self.assertTrue('ports' in instance)
         self.assertEquals('completed', job['status'])
         self.assertTrue(isinstance(instance['task_id'], unicode))
+
+
+    # TODO This method should schedule a job on Cook to determine
+    # what Cook thinks the current user is.  The user is used as
+    # a query parameter when listing jobs, for example.
+    def determine_user(self):
+        return "vagrant"
+
+    def test_list_jobs_by_state(self):
+        # schedule a bunch of jobs in hopes of getting jobs into different statuses
+        request_body = {'jobs': [self.minimal_job(command="sleep %s" % i) for i in range(1,20)]}
+        resp = self.session.post('%s/rawscheduler' % self.cook_url, json=request_body)
+        self.assertEqual(resp.status_code, 201)
+
+        # let some jobs get scheduled
+        time.sleep(10)
+        user = self.determine_user()
+
+        for state in ['waiting', 'running', 'completed']:
+            resp = self.session.get('%s/list?user=%s&state=%s' % (self.cook_url, user, state))
+            self.assertEqual(200, resp.status_code)
+            jobs = resp.json()
+            for job in jobs:
+                #print "%s %s" % (job['uuid'], job['status'])
+                self.assertEquals(state, job['status'])
+
+    # load a job by UUID using GET /rawscheduler
+    def get_job(self, uuid):
+        return self.session.get('%s/rawscheduler?job=%s' % (self.cook_url, uuid)).json()[0]
+
+    def test_list_jobs_by_time(self):
+        # schedule two jobs with different submit times
+        job_specs = [self.minimal_job() for _ in range(2)];
+
+        request_body = {'jobs': [job_specs[0]]}
+        resp = self.session.post('%s/rawscheduler' % self.cook_url, json=request_body)
+        self.assertEqual(resp.status_code, 201)
+
+        time.sleep(1)
+
+        request_body = {'jobs': [job_specs[1]]}
+        resp = self.session.post('%s/rawscheduler' % self.cook_url, json=request_body)
+        self.assertEqual(resp.status_code, 201)
+
+        submit_times = [self.get_job(job_spec['uuid'])['submit_time'] for job_spec in job_specs]
+
+        user = self.determine_user()
+
+        # start-ms and end-ms are exclusive
+
+        # query where start-ms and end-ms are the submit times of jobs 1 & 2 respectively
+        resp = self.session.get('%s/list?user=%s&state=waiting&start-ms=%s&end-ms=%s'
+                % (self.cook_url, user, submit_times[0]-1, submit_times[1]+1))
+        self.assertEqual(200, resp.status_code)
+        jobs = resp.json()
+        self.assertTrue(any(job for job in jobs if job['uuid'] == job_specs[0]['uuid']))
+        self.assertTrue(any(job for job in jobs if job['uuid'] == job_specs[1]['uuid']))
+
+        # query just for job 1
+        resp = self.session.get('%s/list?user=%s&state=waiting&start-ms=%s&end-ms=%s'
+                % (self.cook_url, user, submit_times[0]-1, submit_times[1]))
+        self.assertEqual(200, resp.status_code)
+        jobs = resp.json()
+        self.assertTrue(any(job for job in jobs if job['uuid'] == job_specs[0]['uuid']))
+        self.assertFalse(any(job for job in jobs if job['uuid'] == job_specs[1]['uuid']))
+
+        # query just for job 2
+        resp = self.session.get('%s/list?user=%s&state=waiting&start-ms=%s&end-ms=%s'
+                % (self.cook_url, user, submit_times[0], submit_times[1]+1))
+        self.assertEqual(200, resp.status_code)
+        jobs = resp.json()
+        self.assertFalse(any(job for job in jobs if job['uuid'] == job_specs[0]['uuid']))
+        self.assertTrue(any(job for job in jobs if job['uuid'] == job_specs[1]['uuid']))
+
+        # query for neither
+        resp = self.session.get('%s/list?user=%s&state=waiting&start-ms=%s&end-ms=%s'
+                % (self.cook_url, user, submit_times[0], submit_times[1]))
+        self.assertEqual(200, resp.status_code)
+        jobs = resp.json()
+        self.assertFalse(any(job for job in jobs if job['uuid'] == job_specs[0]['uuid']))
+        self.assertFalse(any(job for job in jobs if job['uuid'] == job_specs[1]['uuid']))
 
     def test_cancel_job(self):
         job_spec = self.minimal_job(command='sleep 300')
