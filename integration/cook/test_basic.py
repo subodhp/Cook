92f0514e95ce3903e98dfdec4271713ba3cadf27
Paul Schorfheide <paul.schorfheide@twosigma.com>
2017-03-27 13:30:40 -0500

    Stop passing uuid

--- integration/cook/test_basic.py
+++ integration/cook/test_basic.py
@@ -21,6 +21,7 @@ class CookTest(unittest.TestCase):
             'max_retries': 1,
             'mem': 100,
             'cpus': 1,
+            'uuid': str(uuid.uuid4()),
             'command': 'echo hello',
             'name': 'echo',
             'priority': 1
@@ -33,20 +34,19 @@ class CookTest(unittest.TestCase):
         self.session = requests.Session()
 
     def test_basic_submit(self):
-        job_uuid = str(uuid.uuid4())
-        request_body = {'jobs': [ self.minimal_job(uuid=job_uuid) ]}
+        job_spec = self.minimal_job()
+        request_body = {'jobs': [ job_spec ]}
         resp = self.session.post('%s/rawscheduler' % self.cook_url, json=request_body)
         self.assertEqual(resp.status_code, 201)
-        job = self.wait_for_job(job_uuid, 'completed')
+        job = self.wait_for_job(job_spec['uuid'], 'completed')
         self.assertEqual('success', job['instances'][0]['status'])
 
     def test_failing_submit(self):
-        job_uuid = str(uuid.uuid4())
-        jobspec = self.minimal_job(uuid=job_uuid, command='exit 1')
+        job_spec = self.minimal_job(command='exit 1')
         resp = self.session.post('%s/rawscheduler' % self.cook_url,
-                                 json={'jobs': [jobspec]})
+                                 json={'jobs': [job_spec]})
         self.assertEqual(201, resp.status_code)
-        job = self.wait_for_job(job_uuid, 'completed')
+        job = self.wait_for_job(job_spec['uuid'], 'completed')
         self.assertEqual(1, len(job['instances']))
         self.assertEqual('failed', job['instances'][0]['status'])
 
@@ -65,12 +65,11 @@ class CookTest(unittest.TestCase):
     #         self.assertEqual('failed', instance['status'])
 
     def test_max_runtime_exceeded(self):
-        job_uuid = str(uuid.uuid4())
-        jobspec = self.minimal_job(uuid=job_uuid, command='sleep 60', max_runtime=5000)
+        job_spec = self.minimal_job(command='sleep 60', max_runtime=5000)
         resp = self.session.post('%s/rawscheduler' % self.cook_url,
-                                 json={'jobs': [jobspec]})
+                                 json={'jobs': [job_spec]})
         self.assertEqual(201, resp.status_code)
-        job = self.wait_for_job(job_uuid, 'completed')
+        job = self.wait_for_job(job_spec['uuid'], 'completed')
         self.assertEqual(1, len(job['instances']))
         self.assertEqual('failed', job['instances'][0]['status'])
 
