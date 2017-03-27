6a824e1554dc31edd96c233946be79882890485b
Paul Schorfheide <paul.schorfheide@twosigma.com>
2017-03-27 13:10:51 -0500

    Refactor minimal_job

--- integration/cook/test_basic.py
+++ integration/cook/test_basic.py
@@ -15,34 +15,33 @@ class CookTest(unittest.TestCase):
         self.assertEqual(status, job['status'])
         return job
 
-    def minimal_job(self, uuid):
-        return {
+    def minimal_job(self, **kwargs):
+        job = {
             'max_retries': 1,
             'mem': 100,
             'cpus': 1,
-            'uuid': str(uuid),
             'command': 'echo hello',
             'name': 'echo',
             'priority': 1
         }
-
+        job.update(kwargs)
+        return job
 
     def setUp(self):
         self.cook_url = 'http://localhost:12321'
         self.session = requests.Session()
 
     def test_basic_submit(self):
-        job_uuid = uuid.uuid4()
-        request_body = {'jobs': [ self.minimal_job(job_uuid) ]}
+        job_uuid = str(uuid.uuid4())
+        request_body = {'jobs': [ self.minimal_job(uuid=job_uuid) ]}
         resp = self.session.post('%s/rawscheduler' % self.cook_url, json=request_body)
         self.assertEqual(resp.status_code, 201)
         job = self.wait_for_job(job_uuid, 'completed')
         self.assertEquals('success', job['instances'][0]['status'])
 
     def test_failing_submit(self):
-        job_uuid = uuid.uuid4()
-        jobspec = self.minimal_job(job_uuid)
-        jobspec['command'] = 'exit 1'
+        job_uuid = str(uuid.uuid4())
+        jobspec = self.minimal_job(uuid=job_uuid, command='exit 1')
         resp = self.session.post('%s/rawscheduler' % self.cook_url,
                                  json={'jobs': [jobspec]})
         self.assertEquals(201, resp.status_code)
@@ -51,7 +50,7 @@ class CookTest(unittest.TestCase):
         self.assertEquals('failed', job['instances'][0]['status'])
 
     # def test_failing_submit_with_retries(self):
-    #     job_uuid = uuid.uuid4()
+    #     job_uuid = str(uuid.uuid4())
     #     print job_uuid
     #     jobspec = self.minimal_job(job_uuid)
     #     jobspec['command'] = 'exit 1'
@@ -65,10 +64,8 @@ class CookTest(unittest.TestCase):
     #         self.assertEquals('failed', instance['status'])
 
     def test_max_runtime_exceeded(self):
-        job_uuid = uuid.uuid4()
-        jobspec = self.minimal_job(job_uuid)
-        jobspec['command'] = 'sleep 60'
-        jobspec['max_runtime'] = 5000
+        job_uuid = str(uuid.uuid4())
+        jobspec = self.minimal_job(uuid=job_uuid, command='sleep 60', max_runtime=5000)
         resp = self.session.post('%s/rawscheduler' % self.cook_url,
                                  json={'jobs': [jobspec]})
         self.assertEquals(201, resp.status_code)
